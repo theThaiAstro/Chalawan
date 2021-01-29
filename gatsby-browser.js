4e0ea7aa3f94d2d710627e14a917bcccf25053a9
Thammarith <thammarith@users.noreply.github.com>
2021-01-29 22:54:38 +0700

    Add fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
-import './src/styles/normalise.scss';
+// import './src/styles/normalise.scss';
 import './src/styles/style.scss';
+import './src/styles/typography.scss';
 
 // Highlighting for code blocks
 import 'prismjs/themes/prism.css';
