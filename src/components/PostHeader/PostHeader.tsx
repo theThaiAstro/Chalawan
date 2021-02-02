7024fbc01257c6496c6901a5629b12d9cb585e34
Thammarith <8011543+thammarith@users.noreply.github.com>
2021-02-02 21:40:01 +0700

    [Feat] Add details and styling to post page (#4)
    * Add initial type settings
    
    * Create post page
    
    Co-authored-by: Thammarith <thammarith@users.noreply.github.com>

--- src/components/PostHeader/PostHeader.tsx
+++ src/components/PostHeader/PostHeader.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Post from '../../types/post';
 
+import styles from './PostHeader.module.scss';
+
 type PostHeaderProps = Pick<Post, 'title' | 'date'>;
 
 const PostHeader: React.FC<PostHeaderProps> = ({ date, title }) => (
-	<header>
-		<h1 itemProp="headline">{title}</h1>
-		<p>{date}</p>
+	<header className={styles.postHeader}>
+		<h1 className={styles.postTitle} itemProp="headline">
+			{title}
+		</h1>
+		{/* <p>{date}</p> */}
 	</header>
 );
 
